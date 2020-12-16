1eafe0c3560f859b0405df2d2c49a2e9260b7898
Mehul Chaudhari <chaudharimehul2001@gmail.com>
2020-12-16 22:18:57 +0530

    Final Commit

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-var btnTranslate = document.querySelector('#btn-translate');
-var txtInput = document.querySelector('btn-input');
-var OutputDiv = document.querySelector('#output');
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/vulcan.json";
 
 function getTranslationURL(text){
-    return serverURL + "?" +"text=" + txtInput;
+   return serverURL + "?" +"text=" + text;
 }
-
 function errorHandler(error){
-    console.log("Hey there's error occured", error);
+   console.log("error occured", error );
 }
 
 function clickHandler() {
-    var inputText = txtInput.value;
+   var inputText = txtInput.value; //taking input
 
-    fetch(getTranslationURL(inputText))
-    .then(response => response.json())
-    .then(json => {
-        var translatedText=  json.contents.translated;
-        OutputDiv.innerText = translatedText;
-    })
-    .catch(errorHandler)
-}
+   //calling server for processing 
+   fetch(getTranslationURL(inputText))
+   .then(response => response.json())
+   .then(json => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+   })
+   .catch(errorHandler)
+};
 btnTranslate.addEventListener("click", clickHandler)
